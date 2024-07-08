5328980cec94ac2675eaee94c374e504e2ad51a3
Bracka1337 <d3adselium@gmail.com>
2024-07-08 16:06:12 +0300

    Auth functionality

--- laravel/app/Http/Controllers/Api/AuthController.php
+++ laravel/app/Http/Controllers/Api/AuthController.php
@@ -4,8 +4,65 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     //
+    public function register(Request $request) {
+        $request->validate([
+            
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => 'User'
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'message'=> 'User Created Successfully'
+        ]);
+    }
+
+    public function login(Request $request) {
+        $request->validate([
+            'email'=> 'required|email',
+            'password'=> 'required'
+        ]);
+
+        if (Auth::attempt([
+            'email'=> $request->email,
+            'password'=> $request->password,
+        ])){
+            $user = Auth::user();
+            $token = $user->createToken('myToken')->accessToken;
+
+            return response()->json([
+                'status'=> true,
+                'message'=> 'Login Successful',
+                'token'=> $token,
+            ]);
+        } else {
+            return response()->json([
+                'status'=> false,
+                'message'=> 'Invalid credentials'
+            ]);
+        }
+    }
+
+    public function logout() {
+        $user = Auth::user();
+
+        auth()->user()->token()->revoke();
+
+        return response()->json([
+            'status'=> true,
+            'message'=> 'Logged Out'
+        ]);  
+    }   
 }
