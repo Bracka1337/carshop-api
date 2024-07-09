611975f7aafc761b6d7d548c0cee31e2d8947e26
Vitalijs Mikutis <vitalijsmikutis@gmail.com>
2024-07-09 09:10:23 +0300

    fixes

--- app/Models/Product_quantity.php
+++ app/Models/Product_quantity.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ProductQuantity extends Model
+class Product_quantity extends Model
 {
     use HasFactory;
 
