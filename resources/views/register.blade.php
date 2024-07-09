3f5435196320ec8bcb2987da1934132cf8e74b1e
Davis <davislak777@gmail.com>
2024-07-09 11:21:57 +0300

    Fix auth to register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         @vite('resources/css/app.css')
-         <title>Authorization</title>
+         <title>Register</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
