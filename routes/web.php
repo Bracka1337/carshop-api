da039a017fa7d6c230cda5460a4c023fc5288eb6
Bracka1337 <d3adselium@gmail.com>
2024-07-09 16:51:14 +0300

    quick fix

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\ProductController;
 
 /*
 |--------------------------------------------------------------------------
