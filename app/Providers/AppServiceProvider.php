1704aa48fabd278641ca6fb0bcfcc553a3f78d09
Vitalijs Mikutis <vitalijsmikutis@gmail.com>
2024-07-09 11:42:38 +0300

    Revert "layout file"
    This reverts commit 459f42457b76efec27c096bcec37fcab00e21238.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Blade::component('layouts.app', 'app-layout');
+        //
     }
 }
