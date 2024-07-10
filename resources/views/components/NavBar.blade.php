5ee5113637848effb13c069adf6ddfd4064716e0
Artjoms <developerjefremov@gmail.com>
2024-07-10 09:10:44 +0300

    NavBar

--- resources/views/components/NavBar.blade.php
+++ resources/views/components/NavBar.blade.php
@@ -65,7 +65,7 @@
   
             <div class="ml-auto flex items-center">
               <div class="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
-                <a href="#" class="text-sm font-medium text-gray-700 hover:text-gray-800">Sign in</a>
+                <a href="{{ route('login') }}" class="text-sm font-medium text-gray-700 hover:text-gray-800">Sign in</a>
                 <span class="h-6 w-px bg-gray-200" aria-hidden="true"></span>
               </div>
 
