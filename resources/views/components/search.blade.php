20f302270faaa65d80ee51144cd039871425a0e9
Deniss Maksimovs <deniss.maksimovs2@inbox.lv>
2024-07-10 09:52:14 +0300

    fetchecd search params

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -1,66 +1,46 @@
+@props(['categories', 'brands', 'sizes', 'price_range'])
+
 <div class="search">
     <div class="flex items-center justify-center mt-40">
-        
         <div class="bg-white p-6 rounded-lg shadow-lg grid grid-cols-4 gap-2 w-full max-w-5xl">
             <div class="p-4">
                 <label for="category" class="block mt-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
                 <select id="category" class="block mt-2 w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 hover:shadow-md">
-                  {{-- category --}}
-                <option selected>Choose a category</option>
-                  <option value="US">United States</option>
-                  <option value="CA">Canada</option>
-                  <option value="FR">France</option>
-                  <option value="DE">Germany</option>
+                    <option selected>Choose a category</option>
+                    @foreach($categories as $category)
+                        <option value="{{ $category->id }}">{{ $category->title }}</option>
+                    @endforeach
                 </select>
             </div>
             <div class="p-4">
                 <label for="brand" class="block mt-2 text-sm font-medium text-gray-900 dark:text-white">Brand</label>
                 <select id="brand" class="block mt-2 w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 hover:shadow-md">
-                  {{-- brand --}}
-                <option selected>Choose a brand</option>
-                  <option value="US">United States</option>
-                  <option value="CA">Canada</option>
-                  <option value="FR">France</option>
-                  <option value="DE">Germany</option>
-                </select>
-            </div>
-            <div class="p-4">
-                <label for="material" class="block mt-2 text-sm font-medium text-gray-900 dark:text-white">Material</label>
-                <select id="material" class="block mt-2 w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 hover:shadow-md">
-                  {{-- material --}}
-                <option selected>Choose a material</option>
-                  <option value="US">United States</option>
-                  <option value="CA">Canada</option>
-                  <option value="FR">France</option>
-                  <option value="DE">Germany</option>
+                    <option selected>Choose a brand</option>
+                    @foreach($brands as $brand)
+                        <option value="{{ $brand->id }}">{{ $brand->title }}</option>
+                    @endforeach
                 </select>
             </div>
             <div class="p-4">
                 <label for="size" class="block mt-2 text-sm font-medium text-gray-900 dark:text-white">Size</label>
                 <select id="size" class="block mt-2 w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 hover:shadow-md">
-                    {{-- size --}}
-                <option selected>Choose a size</option>
-                    <option value="US">United States</option>
-                    <option value="CA">Canada</option>
-                    <option value="FR">France</option>
-                    <option value="DE">Germany</option>
+                    <option selected>Choose a size</option>
+                    @foreach($sizes as $size)
+                        <option value="{{ $size->size }}">{{ $size->size }}</option>
+                    @endforeach
                 </select>
             </div>
             <div class="p-4">
-                <div>
-                    <label for="priceFrom" class="block text-sm font-semibold leading-6 text-gray-900">Price from:</label>
-                    <div class="mt-2.5">
-                      <input type="text" name="priceFrom" id="priceFrom" autocomplete="given-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 hover:shadow-md">
-                    </div>
-                  </div>
+                <label for="priceFrom" class="block text-sm font-semibold leading-6 text-gray-900">Price from:</label>
+                <div class="mt-2.5">
+                    <input type="number" name="priceFrom" id="priceFrom" value="{{ $price_range['min'] }}" autocomplete="given-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 hover:shadow-md">
+                </div>
             </div>
             <div class="p-4">
-                <div>
-                    <label for="priceTo" class="block text-sm font-semibold leading-6 text-gray-900">Price to:</label>
-                    <div class="mt-2.5">
-                      <input type="text" name="priceTo" id="priceTo" autocomplete="given-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 hover:shadow-md">
-                    </div>
-                  </div>
+                <label for="priceTo" class="block text-sm font-semibold leading-6 text-gray-900">Price to:</label>
+                <div class="mt-2.5">
+                    <input type="number" name="priceTo" id="priceTo" value="{{ $price_range['max'] }}" autocomplete="given-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 hover:shadow-md">
+                </div>
             </div>
             <div class="p-4 col-start-4">
                 <div class="mt-8">
@@ -69,5 +49,4 @@
             </div>
         </div>
     </div>
-
- </div>
+</div>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,7 +1,14 @@
 <x-app-layout>
 
-@include('components.search')
-@include('components.grid')
+    @include('components.search', [
+        'categories' => $categories,
+        'brands' => $brands,
+        'sizes' => $sizes,
+        'price_range' => $price_range,
+    ])
+    @include('components.grid')
+
+
 
 </x-app-layout>
 
