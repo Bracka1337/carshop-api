b08260a1fbfbd353ed9e842079f81378123c7790
Artjoms <developerjefremov@gmail.com>
2024-07-10 16:39:11 +0300

    shopping cart opening closing

--- public/js/app.js
+++ public/js/app.js
@@ -1,33 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
     // homepage modal window
     
-    const products = document.querySelectorAll("#product");
-    const modal = document.querySelector("#modal");
-    const productsDetail = document.querySelectorAll("#productDetail");
+    // const products = document.querySelectorAll("#product");
+    // const modal = document.querySelector("#modal");
+    // const productsDetail = document.querySelectorAll("#productDetail");
 
-    products.forEach((product) => {
-        product.addEventListener('click', ((event) => {
-            event.preventDefault();
-            const productId = event.currentTarget.getAttribute('product-id');
-            const productsDetailArr = Array.from(productsDetail);
-            const productDetail = productsDetailArr.find((detail) => {
-                const detailId =detail.getAttribute('product-data-id');
-                return detailId === productId;
-            });
-            modal.classList.remove('hidden');
-            productDetail.classList.remove('hidden');
-        }));
-    });
+    // products.forEach((product) => {
+    //     product.addEventListener('click', ((event) => {
+    //         event.preventDefault();
+    //         const productId = event.currentTarget.getAttribute('product-id');
+    //         const productsDetailArr = Array.from(productsDetail);
+    //         const productDetail = productsDetailArr.find((detail) => {
+    //             const detailId =detail.getAttribute('product-data-id');
+    //             return detailId === productId;
+    //         });
+    //         modal.classList.remove('hidden');
+    //         productDetail.classList.remove('hidden');
+    //     }));
+    // });
 
-    modal.addEventListener('click', ((event) => {
-        event.preventDefault();
+    // modal.addEventListener('click', ((event) => {
+    //     event.preventDefault();
 
-        const productsDetailArr = Array.from(productsDetail);
-        console.log(productsDetailArr);
-        // const productDetail = productsDetailArr.find((detail) => {
-        //     return detail.classList.contains;
-        // });
-    }));
+    //     const productsDetailArr = Array.from(productsDetail);
+    //     console.log(productsDetailArr);
+    //     // const productDetail = productsDetailArr.find((detail) => {
+    //     //     return detail.classList.contains;
+    //     // });
+    // }));
 
     // productsDetail.forEach((detail) => {
     //     detail.addEventListener('click', ((event) => {
@@ -40,5 +40,45 @@ document.addEventListener('DOMContentLoaded', function() {
     //     }));
     // });
 
+//shopping cart
+    //shopping cart close button 
+
+   
+    const closeButton = document.getElementById('close-button');
+    const shoppingCart = document.getElementById('shopping-cart');
+    const modal = document.getElementById('modal');
+    closeButton.addEventListener('click', (event) => {
+    event.preventDefault();
+        shoppingCart.classList.add('hidden');
+        modal.classList.add('hidden');
+    });
+    
+  
+    //shopping cart open button 
+
+    const openButton = document.getElementById('open-button');
+    openButton.addEventListener('click', (event) => {
+        event.preventDefault();
+        modal.classList.remove('hidden');
+        shoppingCart.classList.remove('hidden');
+    });
+
+    
+    
+    modal.addEventListener('click', (event) => {
+        event.preventDefault()
+        
+        if (!shoppingCart.contains(event.target)) {
+            shoppingCart.classList.add('hidden');
+            modal.classList.add('hidden');
+        }
+    })
+
+    const continueShopping = document.getElementById('continue-shopping');
+    continueShopping .addEventListener('click', (event) => {
+        event.preventDefault();
+        shoppingCart.classList.add('hidden');
+        modal.classList.add('hidden');
+    });
 
 });
--- resources/views/components/cart.blade.php
+++ resources/views/components/cart.blade.php
@@ -1,36 +1,18 @@
-<div class="relative z-10" aria-labelledby="slide-over-title" role="dialog" aria-modal="true">
-    <!--
-      Background backdrop, show/hide based on slide-over state.
-  
-      Entering: "ease-in-out duration-500"
-        From: "opacity-0"
-        To: "opacity-100"
-      Leaving: "ease-in-out duration-500"
-        From: "opacity-100"
-        To: "opacity-0"
-    -->
+<div class="relative z-10 hidden" aria-labelledby="slide-over-title" role="dialog" aria-modal="true" id="modal">
+   
     <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
   
     <div class="fixed inset-0 overflow-hidden">
       <div class="absolute inset-0 overflow-hidden">
         <div class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
-          <!--
-            Slide-over panel, show/hide based on slide-over state.
-  
-            Entering: "transform transition ease-in-out duration-500 sm:duration-700"
-              From: "translate-x-full"
-              To: "translate-x-0"
-            Leaving: "transform transition ease-in-out duration-500 sm:duration-700"
-              From: "translate-x-0"
-              To: "translate-x-full"
-          -->
+          
           <div class="pointer-events-auto w-screen max-w-md">
-            <div class="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+            <div class="flex h-full flex-col overflow-y-scroll bg-white shadow-xl hidden" id="shopping-cart">
               <div class="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
                 <div class="flex items-start justify-between">
                   <h2 class="text-lg font-medium text-gray-900" id="slide-over-title">Shopping cart</h2>
                   <div class="ml-3 flex h-7 items-center">
-                    <button type="button" class="relative -m-2 p-2 text-gray-400 hover:text-gray-500">
+                    <button id="close-button" type="button" class="relative -m-2 p-2 text-gray-400 hover:text-gray-500">
                       <span class="absolute -inset-0.5"></span>
                       <span class="sr-only">Close panel</span>
                       <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
@@ -110,7 +92,7 @@
                 <div class="mt-6 flex justify-center text-center text-sm text-gray-500">
                   <p>
                     or
-                    <button type="button" class="font-medium text-indigo-600 hover:text-indigo-500">
+                    <button id="continue-shopping" type="button" class="font-medium text-indigo-600 hover:text-indigo-500">
                       Continue Shopping
                       <span aria-hidden="true"> &rarr;</span>
                     </button>
@@ -123,4 +105,5 @@
       </div>
     </div>
   </div>
+  
   
