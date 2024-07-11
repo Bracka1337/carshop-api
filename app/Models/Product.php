878d899725371d9a0ad365cd7e3275dcadc93a6a
Bracka1337 <d3adselium@gmail.com>
2024-07-11 11:47:41 +0300

    Adjustments

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -12,7 +12,6 @@ class Product extends Model
     protected $fillable = [
         'title',
         'description',
-        'preview_img_uri',
         'material',
         'diameter',
         'width',
