1ac711cc61e4e837467812ab217c66df70a45ae8
Artjoms <developerjefremov@gmail.com>
2024-07-11 09:34:30 +0300

    premerge

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,3 +1,19 @@
 <div>
-    <!-- You must be the change you wish to see in the world. - Mahatma Gandhi -->
+    <div id="dynamicBanner" class="bg-blue-500 text-white p-4">
+        <h2 id="bannerTitle">The Open Network</h2>
+        <p id="bannerSubtitle"></p>
+      </div>
+      <h1 class="Banner__title">
+        <div>The Open Network</div>
+        <span class="Banner__title--second" style="--gradientColor: linear-gradient(88.93deg, #2A82EB 18.91%, #288BEE 79.91%);">
+            for
+        </span>&nbsp;
+        <div style="--gradientColor: linear-gradient(88.93deg, #2689E8 18.91%, #03CFC2 79.91%);" class="Banner__title--second">
+            <div style="display: inline;">
+                <span class="index-module_type__E-SaG wrapper">
+                    start
+                </span>
+            </div>
+        </div>
+    </h1>
 </div>
