af9786780ae700b27525e09b6e02dc87be0f3192
Vitalijs Mikutis <vitalijsmikutis@gmail.com>
2024-07-11 11:08:33 +0300

    separate function

--- app/Http/Controllers/Api/MainController.php
+++ app/Http/Controllers/Api/MainController.php
@@ -12,42 +12,46 @@ class MainController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $query = Product::query();
-    
-        if ($request->has('title') && !empty($request->title)) {
-            $query->where('title', 'like', "%{$request->title}%");
-        }
-    
-        if ($request->has('category_id') && !empty($request->category_id)) {
-            $query->where('category_id', $request->category_id);
-        }
-    
-        if ($request->has('brand_id') && !empty($request->brand_id)) {
-            $query->where('brand_id', $request->brand_id);
-        }
-    
-        if ($request->has('price_from') && !empty($request->price_from)) {
-            $query->where('price', '>=', $request->price_from);
-        }
-    
-        if ($request->has('price_to') && !empty($request->price_to)) {
-            $query->where('price', '<=', $request->price_to);
-        }
-    
-        if ($request->has('size') && !empty($request->size)) {
-            $query->where('size', $request->size);
-        }
-    
-        $initialProducts = $query->paginate(10)->withQueryString();
-    
+        $initialProducts = $this->filterProducts($request)->paginate(10)->withQueryString();
+
         $searchParameters = $this->getSearchParameters();
-    
+
         return view('main', [
             'search' => $searchParameters,
             'initialProducts' => $initialProducts
         ]);
     }
-    
+
+    private function filterProducts(Request $request)
+    {
+        $query = Product::query();
+
+        if ($request->has('title') && !empty($request->title)) {
+            $query->where('title', 'like', "%{$request->title}%");
+        }
+
+        if ($request->has('category_id') && !empty($request->category_id)) {
+            $query->where('category_id', $request->category_id);
+        }
+
+        if ($request->has('brand_id') && !empty($request->brand_id)) {
+            $query->where('brand_id', $request->brand_id);
+        }
+
+        if ($request->has('price_from') && !empty($request->price_from)) {
+            $query->where('price', '>=', $request->price_from);
+        }
+
+        if ($request->has('price_to') && !empty($request->price_to)) {
+            $query->where('price', '<=', $request->price_to);
+        }
+
+        if ($request->has('size') && !empty($request->size)) {
+            $query->where('size', $request->size);
+        }
+
+        return $query;
+    }
 
     public function getSearchParameters()
     {
@@ -77,5 +81,3 @@ class MainController extends Controller
         return $products;
     }
 }
-
-
