ec30872b939a004617f215a79aa39539f15c53f2
Vitalijs Mikutis <vitalijsmikutis@gmail.com>
2024-07-11 12:00:45 +0300

    search adjusted to new model

--- app/Http/Controllers/Api/MainController.php
+++ app/Http/Controllers/Api/MainController.php
@@ -5,13 +5,13 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
-use App\Models\Category;
 use App\Models\Brand;
 
 class MainController extends Controller
 {
     public function __invoke(Request $request)
     {
+
         $initialProducts = $this->filterProducts($request)->paginate(10)->withQueryString();
 
         $searchParameters = $this->getSearchParameters();
@@ -30,12 +30,9 @@ class MainController extends Controller
             $query->where('title', 'like', "%{$request->title}%");
         }
 
-        if ($request->has('category_id') && !empty($request->category_id)) {
-            $query->where('category_id', $request->category_id);
-        }
 
-        if ($request->has('brand_id') && !empty($request->brand_id)) {
-            $query->where('brand_id', $request->brand_id);
+        if ($request->has('brand') && !empty($request->brand)) {
+            $query->where('brand_id', $request->brand);
         }
 
         if ($request->has('price_from') && !empty($request->price_from)) {
@@ -50,27 +47,66 @@ class MainController extends Controller
             $query->where('size', $request->size);
         }
 
+        if ($request->has('diameter') && !empty($request->diameter)) {
+            $query->where('diameter', $request->diameter);
+        }
+
+        if ($request->has('width') && !empty($request->width)) {
+            $query->where('width', $request->width);
+        }
+
+        if ($request->has('et') && !empty($request->et)) {
+            $query->where('et', $request->et);
+        }
+
+        if ($request->has('cb') && !empty($request->cb)) {
+            $query->where('cb', $request->cb);
+        }
+
+        if ($request->has('bolt') && !empty($request->bolt)) {
+            $query->where('bolt', $request->bolt);
+        }
+
+        if ($request->has('bolt_diameter') && !empty($request->bolt_diameter)) {
+            $query->where('bolt_diameter', $request->bolt_diameter);
+        }
+
+        if ($request->has('type') && !empty($request->type)) {
+            $query->where('type', $request->type);
+        }
+
         return $query;
     }
 
     public function getSearchParameters()
     {
-        // $categories = Category::select('id', 'title')->distinct('title')->get();
-        $brands = Brand::select(['id', 'title'])->distinct("title")->get();
-        $sizes = Product::select('size')->distinct()->get();
+
+        $diameter = Product::select('diameter')->where('diameter', '>', 0)->distinct()->get();
+        $width = Product::select('width')->where('width', '>', 0)->distinct()->get();
+        $et = Product::select('et')->where('et', '>', 0)->distinct()->get();
+        $cb = Product::select('cb')->where('cb', '>', 0)->distinct()->get();
+        $bolt = Product::select('bolt')->where('bolt', '>', 0)->distinct()->get();
+        $bolt_diameter = Product::select('bolt_diameter')->where('bolt_diameter', '>', 0)->distinct()->get();
+        $type = Product::select('type')->where('type', '>', 0)->distinct()->get();
         $minPrice = Product::min('price');
         $maxPrice = Product::max('price');
+        $brands = Brand::select('id', 'title')->get();
 
-        $searchParametrs =  [
-            // 'categories' => $categories,
+        $searchParameters = [
             'brands' => $brands,
-            'sizes' => $sizes,
+            'diameter' => $diameter,
+            'width' => $width,
+            'et' => $et,
+            'cb' => $cb,
+            'bolt' => $bolt,
+            'bolt_diameter' => $bolt_diameter,
+            'type' => $type,
             'priceRange' => [
                 'min' => $minPrice,
                 'max' => $maxPrice
             ]
         ];
-        return $searchParametrs;
+        return $searchParameters;
     }
 
     public function getInitialProducts(Request $request)
