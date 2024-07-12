8fdf552f737991f4737a483827892659a2ea003a
Deniss Maksimovs <deniss.maksimovs2@inbox.lv>
2024-07-12 17:03:01 +0300

    Created slide show in modal window

--- public/js/app.js
+++ public/js/app.js
@@ -37,6 +37,57 @@ document.addEventListener('DOMContentLoaded', function() {
         }));
     });
 
+    // modal images slideshow
+    const carousels = document.querySelectorAll('[data-carousel-inner]');
+    carousels.forEach(carousel => {
+        const items = carousel.querySelectorAll('[data-carousel-item]');
+        const indicators = carousel.nextElementSibling.querySelectorAll('[data-carousel-slide-to]');
+        const prevButton = carousel.parentElement.querySelector('[data-carousel-prev]');
+        const nextButton = carousel.parentElement.querySelector('[data-carousel-next]');
+        let activeIndex = 0;
+
+        const showItem = (index) => {
+            items.forEach((item, i) => {
+                if (i === index) {
+                    item.classList.remove('hidden');
+                } else {
+                    item.classList.add('hidden');
+                }
+            });
+
+            indicators.forEach((indicator, i) => {
+                if (i === index) {
+                    indicator.classList.add('bg-gray-400');
+                    indicator.classList.remove('bg-gray-300');
+                } else {
+                    indicator.classList.add('bg-gray-300');
+                    indicator.classList.remove('bg-gray-400');
+                }
+            });
+        };
+
+        const prevItem = () => {
+            activeIndex = (activeIndex > 0) ? activeIndex - 1 : items.length - 1;
+            showItem(activeIndex);
+        };
+
+        const nextItem = () => {
+            activeIndex = (activeIndex < items.length - 1) ? activeIndex + 1 : 0;
+            showItem(activeIndex);
+        };
+
+        prevButton.addEventListener('click', prevItem);
+        nextButton.addEventListener('click', nextItem);
+
+        indicators.forEach((indicator, index) => {
+            indicator.addEventListener('click', () => {
+                activeIndex = index;
+                showItem(activeIndex);
+            });
+        });
+        showItem(activeIndex);
+    });
+
 //shopping cart
     //shopping cart close button 
    
--- resources/views/components/grid.blade.php
+++ resources/views/components/grid.blade.php
@@ -60,26 +60,18 @@
                 <div class="basis-2/5 h-11/12 w-full items-center justify-center rounded-lg bg-gray-300 dark:bg-gray-700 mb-4 relative">
                     <div class="relative w-full h-full" data-carousel-inner>
                         <!-- Item 1 -->
-                        <div class="hidden duration-700 ease-in-out w-full h-full" data-carousel-item>
-                            <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
-                                class="object-cover w-full h-full rounded-lg" alt="Slide 1">
-                        </div>
-                        <!-- Item 2 -->
-                        <div class="duration-700 ease-in-out w-full h-full" data-carousel-item>
-                            <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
-                                class="object-cover w-full h-full rounded-lg" alt="Slide 2">
-                        </div>
-                        <!-- Item 3 -->
-                        <div class="hidden duration-700 ease-in-out w-full h-full" data-carousel-item>
-                            <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
-                                class="object-cover w-full h-full rounded-lg" alt="Slide 3">
-                        </div>
+                        @foreach ($product->images as $key => $image)
+                            <div class=" duration-700 ease-in-out w-full h-full" data-carousel-item>
+                                <img src={{ $image->img_uri}}
+                                    class="object-cover w-full h-full rounded-lg" alt="Slide {{$key}}">
+                            </div>
+                        @endforeach
                     </div>
                     <!-- Slider indicators -->
                     <div class="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2 w-max" data-carousel-indicators>
-                        <button type="button" class="w-3 h-3 rounded-full bg-gray-300 hover:bg-gray-400 focus:outline-none focus:bg-gray-400 transition" data-carousel-slide-to="0"></button>
-                        <button type="button" class="w-3 h-3 rounded-full bg-gray-300 hover:bg-gray-400 focus:outline-none focus:bg-gray-400 transition" data-carousel-slide-to="1"></button>
-                        <button type="button" class="w-3 h-3 rounded-full bg-gray-300 hover:bg-gray-400 focus:outline-none focus:bg-gray-400 transition" data-carousel-slide-to="2"></button>
+                        @foreach ($product->images as $key => $image)
+                            <button type="button" class="w-3 h-3 rounded-full bg-gray-300 hover:bg-gray-400 focus:outline-none focus:bg-gray-400 transition" data-carousel-slide-to="{{$key}}"></button>
+                        @endforeach
                     </div>
                     <!-- Slider controls -->
                     <button type="button"
@@ -130,5 +122,6 @@
           </div>
       </div>
   @endforeach
-  
+  {{-- <script src="https://unpkg.com/flowbite@1.4.0/dist/flowbite.js"></script> --}}
+    
 </div>
