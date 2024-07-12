187763fb0ed401cc00784fb3e8dd644fc4fb3b1b
Vitalijs Mikutis <vitalijsmikutis@gmail.com>
2024-07-12 15:28:10 +0300

    asdasda

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -12,4 +12,13 @@
     ])
     @include('components.grid', ['products' => $initialProducts])
 
+    <x-filament::input.wrapper>
+    <x-filament::input.select wire:model="status">
+        
+        <option value="draft">Draft</option>
+        <option value="reviewing">Reviewing</option>
+        <option value="published">Published</option>
+    </x-filament::input.select>
+</x-filament::input.wrapper>
+
 </x-app-layout>
