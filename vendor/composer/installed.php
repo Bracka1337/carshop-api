0d9975b730d01c1da7d9b9049a60afd2512e395c
Artjoms <developerjefremov@gmail.com>
2024-07-16 11:25:47 +0300

    WIP

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'af7a8dabf22d0dc6088b1bbc368b6556b819f247',
+        'reference' => '989868ee06277f130fdca4ebbb39c944a47fcb90',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -562,7 +562,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'af7a8dabf22d0dc6088b1bbc368b6556b819f247',
+            'reference' => '989868ee06277f130fdca4ebbb39c944a47fcb90',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -1099,8 +1099,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '3.0.0',
-                1 => '1.0|2.0|3.0',
+                0 => '1.0|2.0|3.0',
+                1 => '3.0.0',
             ),
         ),
         'psr/simple-cache' => array(
