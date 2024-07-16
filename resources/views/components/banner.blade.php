860d5b8f70bb7f46ff966d0f39530480ef496ada
Artjoms <developerjefremov@gmail.com>
2024-07-16 14:25:59 +0300

    all good

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,9 +1,9 @@
 
     
 
-    <div class=" p-6 flex justify-center text-center mt-28 truncate">
+    <div class=" p-6 flex justify-center text-center mt-28 truncate ">
         
-        <h1 class="Banner__title">
+        <h1 class="Banner__title ">
             <div class="text-7xl font-bold text-center ">The Best Disks</div>
             <div class="text-8xl h-28 font-bold  text-center inline-block text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 via-indigo-500 to-indigo-600">for
             <span class="Banner__title--second text-8xl font-bold  text-center inline-block text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 via-blue-500 to-violet-600"></span> 
