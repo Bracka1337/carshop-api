a7f5a80682ac7fab5252be46abe00cf1de24bf91
Davis <davislak777@gmail.com>
2024-07-16 09:52:45 +0300

    Fix credit card input and dark formating

--- resources/views/components/orderoverview.blade.php
+++ resources/views/components/orderoverview.blade.php
@@ -1,9 +1,9 @@
-<section class="py-8 antialiased dark:bg-gray-900 md:py-16">
+<section class="py-8 antialiased md:py-16">
     <div class="w-full max-w-7xl px-4 md:px-5 lg:px-6 mx-auto">
         <h2 class="font-manrope font-bold text-3xl md:text-4xl leading-10 text-black text-center">
-            <ol class="items-center flex w-full max-w-3xl text-center text-sm font-medium text-gray-500 dark:text-gray-400 sm:text-base">
-                <li class="after:border-1 flex items-center text-primary-700 after:mx-6 after:hidden after:h-1 after:w-full after:border-b after:border-gray-200 dark:text-primary-500 dark:after:border-gray-700 sm:after:inline-block sm:after:content-[''] md:w-full xl:after:mx-10">
-                  <span class="flex items-center after:mx-2 after:text-gray-200 after:content-['/'] dark:after:text-gray-500 sm:after:hidden">
+            <ol class="items-center flex w-full max-w-3xl text-center text-sm font-medium text-gray-500 sm:text-base">
+                <li class="after:border-1 flex items-center text-primary-700 after:mx-6 after:hidden after:h-1 after:w-full after:border-b after:border-gray-200 sm:after:inline-block sm:after:content-[''] md:w-full xl:after:mx-10">
+                  <span class="flex items-center after:mx-2 after:text-gray-200 after:content-['/'] sm:after:hidden">
                     <svg class="me-2 h-4 w-4 sm:h-5 sm:w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.5 11.5 11 14l4-4m6 2a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                     </svg>
@@ -11,8 +11,8 @@
                   </span>
                 </li>
           
-                <li class="after:border-1 flex items-center text-primary-700 after:mx-6 after:hidden after:h-1 after:w-full after:border-b after:border-gray-200 dark:text-primary-500 dark:after:border-gray-700 sm:after:inline-block sm:after:content-[''] md:w-full xl:after:mx-10">
-                  <span class="flex items-center after:mx-2 after:text-gray-200 after:content-['/'] dark:after:text-gray-500 sm:after:hidden">
+                <li class="after:border-1 flex items-center text-primary-700 after:mx-6 after:hidden after:h-1 after:w-full after:border-b after:border-gray-200 sm:after:inline-block sm:after:content-[''] md:w-full xl:after:mx-10">
+                  <span class="flex items-center after:mx-2 after:text-gray-200 after:content-['/'] sm:after:hidden">
                     <svg class="me-2 h-4 w-4 sm:h-5 sm:w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.5 11.5 11 14l4-4m6 2a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                     </svg>
