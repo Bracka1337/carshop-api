e76da5e863a77a99ed27df9e0666a04e080f3517
Bracka1337 <d3adselium@gmail.com>
2024-07-17 14:40:27 +0300

    return errors on register

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -15,24 +15,30 @@ class AuthController extends Controller
     public function showRegister() {
         return view('register');
     }
-
     public function register(Request $request) {
-        $request->validate([
-            'name' => 'required|max:255',
-            'password'=> 'required|confirmed|min:8|max:16',
-            'email'=> 'required|email|unique:users,email|max:255',
-            'phone_nr' => 'required|min:8|max:32',
-        ]);
 
-        User::create([
-            'name' => $request->name,
-            'password' => Hash::make($request->password),
-            'email' => $request->email,
-            'phone_nr' => $request->phone_nr,
-            'role' => 'User',
-        ]);
-
-        return redirect()->route('login')->with('success','Registered Sucessfully!');
+        try {
+            $request->validate([
+                'name' => 'required|max:255',
+                'password'=> 'required|confirmed|min:8|max:16',
+                'email'=> 'required|email|unique:users,email|max:255',
+                'phone_nr' => 'required|min:8|max:32',
+            ]);
+    
+            User::create([
+                'name' => $request->name,
+                'password' => Hash::make($request->password),
+                'email' => $request->email,
+                'phone_nr' => $request->phone_nr,
+                'role' => 'User',
+            ]);
+    
+            return redirect()->route('login')->with('success','Registered Sucessfully!');
+        } catch (\Exception $e) {
+            return back()->withErrors([
+                'default' => $e->getMessage(),
+            ])->withInput($request->only('default'));
+        }
     }
 
     public function showLogin() {
