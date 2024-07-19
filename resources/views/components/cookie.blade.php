4624663c349deab8d00a4726fa453629c19df222
Bracka1337 <d3adselium@gmail.com>
2024-07-19 11:18:38 +0300

    fix

--- resources/views/components/cookie.blade.php
+++ resources/views/components/cookie.blade.php
@@ -1,6 +1,5 @@
 
-<div id="cookie-banner" class="mb-10 ml-10 fixed inset-x-0 bottom-0 z-10 flex items-center justify-start bg:indigo-500 rounded-lg overflow-hidden shadow-xl max-w-sm opacity-0 transition-opacity duration-500 ease-in-out">
-    <div class="p-4">
+<div id="cookie-banner" class="mb-10 ml-10 fixed inset-x-0 bottom-0 z-10 flex items-center justify-start bg-gradient-to-r from-blue-500 to-indigo-500 rounded-lg overflow-hidden shadow-xl max-w-sm opacity-0 transition-opacity duration-500 ease-in-out">    <div class="p-4">
       <h2 class="text-lg font-semibold mb-2 text-white">Hello Traveler 🍪</h2>
       <p class="text-sm mb-4 text-white">
         This website uses cookies to enhance user experience and to analyze performance and traffic on our website.
